dc3f0d4b45495eee14b68c789aadfeac9bf6601d
LinYX <540608491@qq.com>
2016-08-11 17:44:44 +0800

    Update waterflow.js

--- waterflow.js
+++ waterflow.js
@@ -20,61 +20,91 @@
  *                     	  	}
  * @return {[undefined]}
  */
-function water(_option) {
-	// set the the most outside element of waterflow
-	_option.parent.style.width = _option.column * _option.width + (_option.column - 1) * _option.horizontalMargin + 'px';
+var waterflow = (function() {
+	/**
+	 * [match the width of screen]
+	 * @param  {[object]} _option [the same as before]
+	 * @return {[function]} water
+	 */
+	function responsive(_option) {
+		var deviceWidth = document.documentElement.offsetWidth || document.body.offsetWidth,
+			itemWidth = _option.width;
 
-	// init the array of array that used to record the height of each column
-	var col = new Array(_option.column),
-		flowItems = _option.flowItems;
+		var col = Math.floor(deviceWidth / itemWidth);
 
-	// init2: init the height of the column
-	for (var i = 0, len = col.length; i < len; i++) {
-		col[i] = flowItems[i].offsetTop + flowItems[i].offsetHeight;
+		// calculate the true col of the waterflow according to the formula
+		// that compare (the item's width +  the margin between them) with (deviceWidth)
+		_option.column = (col * itemWidth + (col - 1) * _option.horizontalMargin) < deviceWidth ? col : col - 1;
+		water(_option);
 	}
 
-	// reduce the position of the flowitems
-	for (var j = 0, l = flowItems.length; j < l; j++) {
-		if (_option.width) {
-			flowItems[j].style.width = _option.width + 'px';
+	/**
+	 * [set the layout of waterflow]
+	 * @param  {[object]} _option [the same as before]
+	 * @return {[undefined]}
+	 */
+	function water(_option) {
+		// set the the most outside element of waterflow
+		_option.parent.style.width = _option.column * _option.width + (_option.column - 1) * _option.horizontalMargin + 'px';
+
+		// init the array of array that used to record the height of each column
+		var col = new Array(_option.column),
+			flowItems = _option.flowItems;
+
+		// init2: init the height of the column
+		for (var i = 0, len = col.length; i < len; i++) {
+			col[i] = flowItems[i].offsetTop + flowItems[i].offsetHeight;
 		}
 
-		// since the the init2 has configure the height of the first
-		// but u also can init the array from here
-		if (j < _option.column) {
-			flowItems[j].style.top = 0;
-			flowItems[j].style.left = (j % _option.column) * (_option.width + _option.horizontalMargin) + 'px';
-		} else {
-			var minH = Math.min.apply(this, col);
-			var minIndex = col.indexOf(minH);
+		// reduce the position of the flowitems
+		for (var j = 0, l = flowItems.length; j < l; j++) {
+			if (_option.width) {
+				flowItems[j].style.width = _option.width + 'px';
+			}
 
-			flowItems[j].style.left = minIndex * (_option.width + _option.horizontalMargin) + 'px';
-			flowItems[j].style.top = minH + _option.verticalMargin + 'px';
+			// since the the init2 has configure the height of the first
+			// but u also can init the array from here
+			if (j < _option.column) {
+				flowItems[j].style.top = 0;
+				flowItems[j].style.left = (j % _option.column) * (_option.width + _option.horizontalMargin) + 'px';
+			} else {
+				var minH = Math.min.apply(this, col);
+				var minIndex = col.indexOf(minH);
 
-			col[minIndex] += flowItems[j].scrollHeight + _option.verticalMargin;
+				flowItems[j].style.left = minIndex * (_option.width + _option.horizontalMargin) + 'px';
+				flowItems[j].style.top = minH + _option.verticalMargin + 'px';
+
+				col[minIndex] += flowItems[j].scrollHeight + _option.verticalMargin;
+			}
 		}
-	}
 
-	// for IE 6-8 without the function [indexOf] in the Array
-	if (!Array.prototype.indexOf) {
-		Array.prototype.indexOf = function(item) {
-			var len = this.length;
+		// for IE 6-8 without the function [indexOf] in the Array
+		if (!Array.prototype.indexOf) {
+			Array.prototype.indexOf = function(item) {
+				var len = this.length;
 
-			if (len <= 0) {
+				if (len <= 0) {
+					return -1;
+				}
+
+				// since indexOf can search from the specific position, default(0)
+				var start = Number(arguments[1]) > 0 ? Number(arguments[1]) : 0 || 0;
+
+				// loop the array and matching the item
+				// i in this -> whether the i(index) in arr or overflow
+				for (var i = start; i < len; i++) {
+					if (i in this && this[i] == item) {
+						return i;
+					}
+				}
 				return -1;
 			}
-
-			// since indexOf can search from the specific position
-			var start = Number(arguments[1]) > 0 ? Number(arguments[1]) : 0 || 0;
-
-			// loop the array and matching the item
-			// i in this -> whether the i(index) in arr or overflow
-			for (var i = start; i < len; i++) {
-				if (i in this && this[i] == item) {
-					return i;
-				}
-			}
-			return -1;
 		}
 	}
-}
+
+	return {
+		fire: responsive
+	}
+})();
+
+
